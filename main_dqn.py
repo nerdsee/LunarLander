821adddfbfb52c9554493aed620e2dc083f4dd88
nerdsee <nerdsee@users.noreply.github.com>
2021-07-30 17:58:45 +0200

    separated DQN. Model Folder generated

--- main_dqn.py
+++ main_dqn.py
@@ -3,14 +3,11 @@ from collections import deque
 
 import gym
 import numpy as np
-import tensorflow.keras.models
-import tensorflow.keras as keras
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import layers
 
 import datetime
+
+from dqn import DQN
+
 e_start = datetime.datetime.now()
 e_end = datetime.datetime.now()
 print(e_start - e_end)
@@ -24,115 +21,6 @@ def tic(step):
 
     mx_now = now
 
-class DQN:
-    def __init__(self, gamma = 0.99, epsilon = 1, epsilon_decay = 0.9, epsilon_min=0.01, first=256, second=256, batch_size=64):
-        # tuning params
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.epsilon_decay = epsilon_decay
-        self.epsilon_min = epsilon_min
-        self.first = first
-        self.second = second
-        self.batch_size = batch_size
-
-        self.state_space = 8
-        self.action_space = 4
-
-
-        self.main_network = self.build_network()
-        self.target_network = self.build_network()
-
-        self.replay_buffer = deque(maxlen=100000)
-
-    def add_sars(self, state, action, reward, state_new, done):
-        self.replay_buffer.append((state, action, reward, state_new, done))
-
-    def get_action(self, state):
-        if random.uniform(0,1) < self.epsilon:
-            return np.random.randint(self.action_space)
-
-        return self.get_action_from_model(state)
-
-    def get_action_from_model(self, state):
-        state = np.reshape(state, (1, 8))
-        qvalues = self.main_network.predict(state)
-        return np.argmax(qvalues[0])
-
-    def build_network(self):
-        model = Sequential()
-        model.add(Dense(self.first, input_dim=self.state_space, activation='relu'))
-        model.add(Dense(self.second, activation='relu'))
-        model.add(Dense(self.action_space, activation='linear'))
-        model.compile(loss='mse', optimizer=Adam())
-        return model
-
-    def reduce_epsilon(self):
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-
-    def train(self):
-
-        # wait until replay buffer fill level is sufficient
-        if len(self.replay_buffer) <= self.batch_size:
-            return
-
-        replay_sample = random.sample(self.replay_buffer, self.batch_size)
-
-        x_train = np.zeros(shape=(self.batch_size, self.state_space))
-        y_train = np.zeros(shape=(self.batch_size, self.action_space))
-
-        sample = 0
-
-        b_state = np.zeros(shape=(self.batch_size, self.state_space))
-        b_state_new = np.zeros(shape=(self.batch_size, self.state_space))
-        for state, action, reward, state_new, done in replay_sample:
-            b_state[sample] = state
-            b_state_new[sample] = state_new
-            sample += 1
-
-        action_batch = self.target_network.predict(b_state_new)
-        q_batch = self.target_network.predict(b_state)
-
-        sample = 0
-        for state, action, reward, state_new, done in replay_sample:
-
-            if done:
-                target_q = reward
-            else:
-                state_new = np.reshape(state_new, (1, 8))
-                actions = action_batch[sample] # self.target_network.predict(state_new)
-                target_q = reward + self.gamma * np.amax(actions)
-
-            state = np.reshape(state, (1, 8))
-            actual_q = q_batch[sample]
-            actual_q = np.reshape(actual_q, (1, 4))
-            # actual_q = self.main_network.predict(state)
-            actual_q[0][action] = target_q
-
-            x_train[sample]=state
-            y_train[sample]=actual_q
-
-            sample += 1
-
-            # self.main_network.fit(state, actual_q, batch_size=128, epochs=1, verbose=0)
-        x_train.reshape(self.batch_size, 8)
-        y_train.reshape(self.batch_size, 4)
-
-        history = self.main_network.fit(x_train, y_train, epochs=1, verbose=0)
-        # history.history
-
-    def load(self, filename, episode=-1):
-        path = filename + '.' + str(episode)
-        self.main_network = tensorflow.keras.models.load_model(path)
-        self.target_network = self.build_network()
-        # self.update_target_network()
-
-    def update_target_network(self):
-        self.target_network.set_weights(self.main_network.get_weights())
-
-    def save(self, filename, episode=-1):
-        path = filename + '.' + str(episode)
-        self.target_network.save(path, overwrite=True)
 
 def train(env, lunar_dqn, num_epsiodes, model_path, do_render = False):
     state = env.reset()
@@ -236,7 +124,7 @@ def fly(env, lunar_dqn, num_epsiodes, record):
 
 env = gym.make("LunarLander-v2")
 
-num_epsiodes = 200
+num_epsiodes = 600
 visible_episodes = 50
 record = False
 
@@ -250,12 +138,15 @@ gamma = 0.99
 epsilon = 1
 epsilon_decay = 0.996
 epsilon_min = 0.01
-first = 256
-second = 256
+first = 160
+second = 140
 batch_size = 64
 
-path_pattern = 'C:/Users/Public/Documents/dev/lunar/{}_{}_g{}_e{}_ed{}_b{}/model'
-path = path_pattern.format(first, second, gamma, epsilon, epsilon_decay, batch_size)
+# path_root = 'C:/Users/Public/Documents/dev/lunar/'
+path_root = 'C:/Users/U429079/models/'
+
+path_pattern = '{}_{}_g{}_e{}_ed{}_b{}/model'
+path = path_root + path_pattern.format(first, second, gamma, epsilon, epsilon_decay, batch_size)
 
 if dotrain:
     lunar_dqn = DQN(gamma=gamma, epsilon=epsilon, epsilon_decay=epsilon_decay, epsilon_min=epsilon_min, first=first, second=second, batch_size=batch_size)
